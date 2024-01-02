85f5d70b4b12a81fd9b68dc75fe16f974b8a5065
Aung Kaung Myat <aungkaungmyatyeu123@gmail.com>
2024-01-02 15:24:41 +0630

    [Complete] atm

--- ATMManagement.MvcApp/Models/UserModel.cs
+++ ATMManagement.MvcApp/Models/UserModel.cs
@@ -13,4 +13,17 @@ namespace ATMManagement.MvcApp.Models
         public int Pin { get; set; }
         public int? Balance { get; set; }
     }
+
+    public class MessageModel
+    {
+        public MessageModel() { }
+
+        public MessageModel(bool isSuccess, string message)
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+        }
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+    }
 }
